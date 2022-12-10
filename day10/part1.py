abb635530804d8b2d1ecca0f7b7045508452790e
Jonas Müller <hej@jonasportfol.io>
2022-12-10 09:02:25 +0100

    Finish day 10

--- day10/part1.py
+++ day10/part1.py
@@ -4,7 +4,7 @@ from common import *
 def find_result(instructions):
     signal_strengths = []
     def f_after(cycle, register): signal_strengths.append(cycle * register)
-    run_instructions(instructions, 20, lambda _: None, f_after)
+    run_instructions(instructions, 20, lambda *_: None, f_after)
     return sum(signal_strengths)
 
 
